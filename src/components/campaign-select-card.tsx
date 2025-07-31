0aede5ca2ffb7f0c231eec1340874d4a07bd4948
Anish <anishwijesinghe@gmail.com>
2025-07-31 15:40:40 +0530

    init

---
+++ src/components/campaign-select-card.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Button } from './ui/button'
+
+export function CampaignSelectCard() {
+  return (
+    <Card>
+      <CardHeader></CardHeader>
+      <CardContent>
+        <Button>Meta</Button>
+      </CardContent>
+    </Card>
+  )
+}
